3ae49f653f2c90adcfe509329e16a98248dfe94b
Nghia Pham <nghia.pq77@gmail.com>
2022-08-11 21:04:21 +0700

    Add pam_auth patch and an install script.

--- config.h
+++ config.h
@@ -1,12 +1,16 @@
 /* user and group to drop privileges to */
-static const char *user  = "nobody";
-static const char *group = "nobody";
+static const char *user  = "oz";
+static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+	[PAM] =    "#9400D3",   /* waiting for PAM */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* PAM service that's used for authentication */
+static const char* pam_service = "login";
